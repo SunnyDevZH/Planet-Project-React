19f238de2232ec33e9a73f3255e77dd63a835c9d
SunnyDevZH <yannick.vaterlaus@windowslive.com>
2025-01-10 11:35:02 +0100

    Ihre Commit-Nachricht

---
+++ Desktop/Coderr-main/mein_backend/accounts/serializers.py
@@ -0,0 +1,31 @@
+# filepath: accounts/serializers.py
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate
+
+User = get_user_model()
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Ungültige Anmeldedaten")
