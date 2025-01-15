a9e43fb33135f0042e24203656b59c717ef728d1
SunnyDevZH <yannick.vaterlaus@windowslive.com>
2025-01-15 13:30:25 +0100

    Deine Commit-Nachricht

---
+++ Desktop/Coderr/mein_backend/accounts/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, viewsets
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import RegistrationSerializer, LoginSerializer, CustomUserSerializer
+from .models import CustomUser
+
+# Create your views here.
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'token': str(refresh.access_token),
+                'username': user.username,
+                'email': user.email,
+                'user_id': user.id
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'token': str(refresh.access_token),
+                'username': user.username,
+                'email': user.email,
+                'user_id': user.id
+            }, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+
+def registration_view(request):
+    return render(request, 'registration.html')
